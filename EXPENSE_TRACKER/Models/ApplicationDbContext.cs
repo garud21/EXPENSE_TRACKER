55236d9b1a3766d061c355a586fe5f716b149589
garud21 <87640570+garud21@users.noreply.github.com>
2023-01-13 21:28:51 +0530

    Add project files.

---
+++ EXPENSE_TRACKER/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EXPENSE_TRACKER.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ EXPENSE_TRACKER/Models/Transaction.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EXPENSE_TRACKER.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //categoryid 
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+
+        public DateTime date { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+
+    }
+}
